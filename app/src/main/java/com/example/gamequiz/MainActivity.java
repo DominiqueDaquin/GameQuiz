cf45f87eae8e3b1319ba07d7609844e5e00d10da
Daki tech <kouakepdominique@gmail.com>
2023-01-28 15:33:13 +0100

    ajout de quelques nouveau fichier. Notamment les layouts et activity des parties jeu solo ,jeu en equipe et jeu en ligne. Egalement l'ajout de l'image parchemin dans le dossier drawable.

--- app/src/main/java/com/example/gamequiz/MainActivity.java
+++ app/src/main/java/com/example/gamequiz/MainActivity.java
@@ -1,14 +1,51 @@
+/*
+**  Created by Dakitech on 28/01/23
+ */
 package com.example.gamequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView solo;
+    private TextView equipe;
+    private TextView ligne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.solo= (TextView) findViewById(R.id.solo);
+        this.equipe= (TextView) findViewById(R.id.equipe);
+        this.ligne= (TextView) findViewById(R.id.ligne);
+
+        solo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent jeusolo=new Intent(getApplicationContext(),soloActivity.class);
+                startActivity(jeusolo);
+                finish();
+            }
+        });
+        equipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent jeuequipe=new Intent(getApplicationContext(),equipeActivity.class);
+                startActivity(jeuequipe);
+                finish();
+            }
+        });
+        ligne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent jeuligne=new Intent(getApplicationContext(),ligneActivity.class);
+                startActivity(jeuligne);
+                finish();
+            }
+        });
     }
 }
