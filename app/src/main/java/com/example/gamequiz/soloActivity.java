ec45f527f51440b2630d5d6f855ae63140a6c174
Daki tech <kouakepdominique@gmail.com>
2023-01-29 22:28:58 +0100

    ajout de quelques nouveau fichier. Notamment les layouts et activity des parties jeu solo ,jeu en equipe et jeu en ligne. Egalement l'ajout de l'image parchemin dans le dossier drawable.

--- app/src/main/java/com/example/gamequiz/soloActivity.java
+++ app/src/main/java/com/example/gamequiz/soloActivity.java
@@ -2,13 +2,32 @@ package com.example.gamequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class soloActivity extends AppCompatActivity {
-
+    private EditText textsolo;
+    private Button btnvalider;
+    private TextView t1;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solo);
+        this.textsolo=(EditText) findViewById(R.id.textsolo);
+        this.btnvalider=(Button) findViewById(R.id.buttonValider);
+
+
+        btnvalider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            t1.setText("yo");
+            }
+        });
     }
 }
